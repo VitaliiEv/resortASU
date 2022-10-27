9a68338eda4e31b2e9df1b3a40b004c71e18d017
Vitalii <solomonov.v@gmail.com>
2022-10-27 22:48:22 +0500

    add l10n support

--- src/main/java/vitaliiev/resortASU/config/SpringConfig.java
+++ src/main/java/vitaliiev/resortASU/config/SpringConfig.java
@@ -2,7 +2,10 @@ package vitaliiev.resortASU.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.Locale;
 
 @Configuration
 public class SpringConfig {
@@ -10,6 +13,15 @@ public class SpringConfig {
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public ResourceBundleMessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setDefaultLocale(Locale.ENGLISH);
+        messageSource.setFallbackToSystemLocale(false);
+        messageSource.setBasename("messages/Messages");
+        return messageSource;
     }
 
 }
