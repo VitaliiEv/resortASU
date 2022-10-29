967308c96bdadf180545825353445fbb463c5e7b
Vitalii <solomonov.v@gmail.com>
2022-10-29 13:28:13 +0500

    fix caching logic

--- src/main/java/vitaliiev/resortASU/service/auth/RoleService.java
+++ src/main/java/vitaliiev/resortASU/service/auth/RoleService.java
@@ -2,20 +2,19 @@ package vitaliiev.resortASU.service.auth;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.CacheConfig;
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.CachePut;
-import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.*;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import vitaliiev.resortASU.entity.auth.Role;
 import vitaliiev.resortASU.repository.auth.RoleRepository;
 
 import java.util.*;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
-@CacheConfig(cacheNames = "roles")
 public class RoleService {
     private final static String DEFAULT_ADMIN = "ROLE_ADMIN";
     private final static String DEFAULT_USER = "ROLE_USER";
@@ -27,7 +26,10 @@ public class RoleService {
         this.roleRepository = roleRepository;
     }
 
-    @CachePut(key = "#result?.id")
+    @Caching(
+            put = {@CachePut(cacheNames = "roles", key = "#result?.id")},
+            evict = {@CacheEvict(cacheNames = "rolesList", allEntries = true)}
+    )
     public Role create(Role role) {
         String name = role.getName();
         if (!name.startsWith("ROLE_")) {
@@ -44,36 +46,65 @@ public class RoleService {
         }
     }
 
-    @Cacheable(key = "#id")
+    @Cacheable(cacheNames = "roles", key = "#id")
     public Role findRoleById(Integer id) {
         return roleRepository.findById(id).orElse(null);
     }
 
-    @Cacheable(key = "#name")
     public Role findRoleByName(String name) {
-        return roleRepository.findByName(name);
+        List<Role> unfilteredList = this.findRoleByNames(new String[] {name});
+        return unfilteredList.stream()
+                .filter(r-> r.getName().equals(name))
+                .findAny().orElse(null); // todo Optional
     }
 
-    public List<Role> findRoleByNames(String[] names) { // fixme
-        List<Role> rolesList = new ArrayList<>();
+    public List<Role> findRoleByNames(String[] names) {
+        return this.findRoleByNames(names, true);
+    }
+
+    public List<Role> findRoleByNames(String[] names, boolean strict) {
+        //todo https://www.baeldung.com/spring-data-criteria-queries
+
         if (names == null || names.length == 0) {
-            return rolesList;
+            return new ArrayList<>();
         }
-        List<String> roleNamesList = Arrays.asList(names);
-        for (String roleName : names) {
-            Role role = roleRepository.findByName(roleName);
-            if (role != null) {
-                rolesList.add(role);
-            }
+        Predicate<Role> matcher;
+        if (strict) {
+            matcher = role -> {
+                for (String roleName : names) {
+                    if (role.getName().equals(roleName)) {
+                        return true;
+                    }
+                }
+                return false;
+            };
+        } else {
+            matcher = role -> {
+                for (String roleName : names) {
+                    if (role.getName().contains(roleName)) {
+                        return true;
+                    }
+                }
+                return false;
+            };
         }
-        return rolesList;
+
+        // assume that roles list is small, and we can fetch it quickly
+        return this.findAll().stream()
+                .filter(matcher)
+                .collect(Collectors.toList());
     }
 
-    public List<Role> findAll() { // FIXME: 28.10.2022
-        return roleRepository.findAll();
+    @Cacheable(cacheNames = "rolesList")
+    public List<Role> findAll() {
+        return roleRepository.findAll(Sort.by("name"));
+//        return roleRepository.findAll();
     }
 
-    @Cacheable(key = "#role.id")
+    @Caching(
+            put = {@CachePut(cacheNames = "roles", key = "#result?.id")},
+            evict = {@CacheEvict(cacheNames = "rolesList", allEntries = true)}
+    )
     public Role update(Role role) {
         if (!role.getEnabled() && (role.getName().equals(DEFAULT_ADMIN) || role.getName().equals(DEFAULT_USER))) {
             log.warn("Can't disable ADMIN or USER built in role.");
@@ -87,7 +118,10 @@ public class RoleService {
         }
     }
 
-    @CacheEvict(key = "#id")
+    @Caching(
+            evict = {@CacheEvict(cacheNames = "roles", key = "#id"),
+                    @CacheEvict(cacheNames = "rolesList", allEntries = true)}
+    )
     public void delete(Integer id) {
         Optional<Role> optionalRole = roleRepository.findById(id);
         optionalRole.ifPresent(r -> {
@@ -100,12 +134,10 @@ public class RoleService {
         });
     }
 
-    @CachePut(key = "#result.id")
     public Role getAdmin() {
         return findRoleByName(DEFAULT_ADMIN);
     }
 
-    @CachePut(key = "#result.id")
     public Role getUser() {
         return findRoleByName(DEFAULT_USER);
     }
