46e880eb8028b7953d256df6d42dd1010ec3c9fe
Vitalii <solomonov.v@gmail.com>
2022-11-10 11:19:19 +0500

    facilities repositories added

---
+++ src/main/java/vitaliiev/resortASU/repository/facilities/BuildingFloorRepository.java
@@ -0,0 +1,7 @@
+package vitaliiev.resortASU.repository.facilities;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import vitaliiev.resortASU.model.facilities.BuildingFloor;
+
+public interface BuildingFloorRepository extends JpaRepository<BuildingFloor, Integer> {
+}
---
+++ src/main/java/vitaliiev/resortASU/repository/facilities/FloorRepository.java
@@ -0,0 +1,7 @@
+package vitaliiev.resortASU.repository.facilities;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import vitaliiev.resortASU.model.facilities.Floor;
+
+public interface FloorRepository extends JpaRepository<Floor, Integer> {
+}
