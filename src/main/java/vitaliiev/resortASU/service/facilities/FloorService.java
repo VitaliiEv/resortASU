8276031199e02d16d22d1c2b095f77ce6a6e85e3
Vitalii <solomonov.v@gmail.com>
2022-11-11 23:03:40 +0600

    added floor service entities

---
+++ src/main/java/vitaliiev/resortASU/service/facilities/FloorService.java
@@ -0,0 +1,101 @@
+package vitaliiev.resortASU.service.facilities;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import vitaliiev.resortASU.model.facilities.Floor;
+import vitaliiev.resortASU.repository.facilities.FloorRepository;
+
+import java.util.List;
+
+@Slf4j
+@Service
+public class FloorService {
+
+
+    protected static final String ENTITY_NAME = Floor.ENTITY_NAME;
+    protected static final String CACHE_NAME = ENTITY_NAME;
+    protected static final String CACHE_LIST_NAME = ENTITY_NAME + "List";
+
+    private static final ExampleMatcher SEARCH_CONDITIONS_MATCH_ALL = ExampleMatcher
+            .matching()
+            .withIncludeNullValues()
+            .withMatcher("floor", ExampleMatcher.GenericPropertyMatchers.contains().ignoreCase())
+            .withIgnorePaths("id", "buildingFloors");
+
+    private final FloorRepository repository;
+
+    @Autowired
+    public FloorService(FloorRepository repository) {
+        this.repository = repository;
+    }
+
+    @Caching(
+            put = {@CachePut(cacheNames = CACHE_NAME, key = "#result?.id")},
+            evict = {@CacheEvict(cacheNames = CACHE_LIST_NAME, allEntries = true)}
+    )
+    public Floor create(Floor entity) throws DataIntegrityViolationException {
+        return repository.save(entity);
+    }
+
+    @Cacheable(cacheNames = CACHE_NAME, key = "#id")
+    public Floor findById(Integer id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    public List<Floor> find(Floor entity) {
+        Example<Floor> example = Example.of(entity, SEARCH_CONDITIONS_MATCH_ALL);
+        return repository.findAll(example, Sort.by("id"));
+    }
+
+    @Cacheable(cacheNames = CACHE_LIST_NAME)
+    public List<Floor> findAll() {
+        return repository.findAll(Sort.by("id"));
+    }
+
+    @Caching(
+            put = {@CachePut(cacheNames = CACHE_NAME, key = "#result?.id")},
+            evict = {@CacheEvict(cacheNames = CACHE_LIST_NAME, allEntries = true)}
+    )
+    public Floor update(Floor entity) {
+        try {
+            return repository.save(entity);
+        } catch (DataIntegrityViolationException e) {
+            log.warn(e.getMessage());
+            return entity;
+        }
+    }
+
+    @Caching(
+            evict = {@CacheEvict(cacheNames = CACHE_NAME, key = "#id"),
+                    @CacheEvict(cacheNames = CACHE_LIST_NAME, allEntries = true)}
+    )
+    public void delete(Integer id) {
+        Floor entity = this.findById(id);
+
+        if (entity.getBuildingFloors() != null &&
+                !entity.getBuildingFloors().isEmpty()) {
+            entity.getBuildingFloors()
+                    .stream()
+                    .filter(bf -> bf.getFloor().equals(entity))
+                    .forEach(bf -> bf.getBuilding().removeBuildingFloor(bf));
+            entity.getBuildingFloors().clear();
+        }
+
+        try {
+            repository.save(entity);
+        } catch (DataIntegrityViolationException e) {
+            log.warn(e.getMessage());
+        }
+
+    }
+
+}
